3713500e19cf9800cdf6e5203982f666935a5937
Sihilel <103405571+Sihilel@users.noreply.github.com>
2022-05-20 18:52:32 +0530

    🦄Hiruu-Queen🦄

--- DarkTeam.js
+++ DarkTeam.js
@@ -1,5 +1,5 @@
 module.exports = {
     HIRUU_QUEEN_OWNER: '94704101989', // DARK Team - HIRUUQUEEN Owner
     DARK_TEAM: '94704101989', // Team - DARK TEAM
-    DARK_OWNER: '94704101989' // SIHILEL DISSANAYAKE - HIRUUQUEEN Owner --> Owner in Sihilel 
+    DARK_OWNER: '94704101989' // SIHILEL DISSANAYAKE - HIRUU-QUEEN Owner --> Owner in Sihilel 
 };
